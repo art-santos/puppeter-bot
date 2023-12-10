4ea998e40d518b0f77d319a8f7c3da20026a7289
Webdev <arthursantosdeveloper@gmail.com>
2023-12-09 23:04:52 -0300

    chore(issue#03:api[API]: all tests working || api endpoint for getting the messages on the server not in memory runtime like before)

---
+++ src/app/api/database/messages/find/get-messages-final/route.ts
@@ -0,0 +1,24 @@
+import supabase from "../../../../../../app/clients/supabaseClient";
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const result = await supabase.from("final_messages").select(`*`);
+    console.log("result", result);
+    console.log("result", result.data);
+
+    if (result.error) {
+      throw new Error(result.error.message);
+    }
+    return NextResponse.json(
+      { data: result, message: "OK", result },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { data: null, message: "Error", error },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/database/messages/find/get_message_ids.test.ts
@@ -0,0 +1,15 @@
+import { NextRequest } from "next/server";
+import { GET } from "./route";
+
+describe("Must get the id of the all message tables in supabase", () => {
+  it("should get the id of the all message tables in supabase", async () => {
+    const response = await GET();
+    const { data } = await response.json();
+
+    console.log("json", data.data);
+
+    expect(data).toBeDefined();
+
+    expect(response.status).toBe(200);
+  });
+});
